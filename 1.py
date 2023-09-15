e59beec14cbf6ab13824a3f9d1cbc94cce156160
CandiedDisc192 <ermodeti@gmail.com>
2023-09-15 15:23:10 +0300

    Task1 and Task2 modified

--- 1.py
+++ 1.py
@@ -1,13 +1,12 @@
 def expression(variable):
-    variable = int(variable) if int(variable) == variable else variable
     square_of_variable = variable * variable
     return (square_of_variable + 1) * (square_of_variable + variable) + 1
 
 
 if __name__ == "__main__":
     try:
-        x = float(input("Calculating x^4 + x^3 + x^2 + x + 1, enter x: "))
-        print("{arg}^4 + {arg}^3 + {arg}^2 + {arg} + 1 = {ans}".format(arg=int(x) if int(x) == x else x,
-                                                                       ans=expression(x)))
+        x = int(input("Calculating x^4 + x^3 + x^2 + x + 1, enter x: "))
+        calculated_result = expression(x)
+        print(f"{x}^4 + {x}^3 + {x}^2 + {x} + 1 = {calculated_result}")
     except ValueError:
         print("Incorrect input.")
--- 2.py
+++ 2.py
@@ -5,39 +5,29 @@ def get_vector_length(vector):
     return sqrt(sum([coord ** 2 for coord in vector]))
 
 
-def get_dot_product(vector1, vector2):
+def get_scalar_product(vector1, vector2):
     return sum([vector1[i] * vector2[i] for i in range(len(vector1))])
 
 
 def get_angle_between_vectors(vector1, vector2):
-    return degrees(acos(get_dot_product(vector1, vector2) / (get_vector_length(vector1) * get_vector_length(vector2))))
+    return degrees(acos(get_scalar_product(vector1, vector2) / (get_vector_length(vector1)
+                                                                * get_vector_length(vector2))))
 
 
 def get_transposed_matrix(matrix, rows_count, columns_count):
-    transposed_matrix = [[0] * rows_count for _ in range(columns_count)]
-    for ri in range(rows_count):
-        for ci in range(columns_count):
-            transposed_matrix[ci][ri] = matrix[ri][ci]
-    return transposed_matrix
+    return [[matrix[j][i] for j in range(rows_count)] for i in range(columns_count)]
 
 
 def get_matrices_sum(matrix1, matrix2, rows_count, columns_count):
-    res = [[0] * columns_count for _ in range(rows_count)]
-    for ri in range(rows_count):
-        for ci in range(columns_count):
-            res[ri][ci] = matrix1[ri][ci] + matrix2[ri][ci]
-    return res
+    return [[matrix1[j][i] + matrix2[j][i] for i in range(columns_count)] for j in range(rows_count)]
 
 
 def get_matrices_product(matrix1, matrix2, rows_count1, columns_count1, rows_count2, columns_count2):
     if columns_count1 != rows_count2:
         return None
-    product = [[0] * columns_count2 for _ in range(rows_count1)]
-    for ri in range(rows_count1):
-        for ci in range(columns_count2):
-            for ti in range(columns_count1):
-                product[ri][ci] += matrix1[ri][ti] * matrix2[ti][ci]
-    return product
+    transposed_matrix2 = get_transposed_matrix(matrix2, rows_count2, columns_count2)
+    return [[get_scalar_product(matrix1[i], transposed_matrix2[j]) for j in range(columns_count2)] for i in
+            range(rows_count1)]
 
 
 def get_vector_from_user(vector_name=""):
@@ -47,15 +37,46 @@ def get_vector_from_user(vector_name=""):
     return processed_vector
 
 
-def get_matrix_info(matrix_name=""):
+def get_matrix_info_from_user(matrix_name=""):
     matrix_rows_count = int(input("Enter number of {}matrix rows: ".format(matrix_name)))
     matrix_columns_count = int(input("Enter number of {}matrix columns: ".format(matrix_name)))
     matrix = []
-    for row_num in range(1, matrix_rows_count+1):
+    for row_num in range(1, matrix_rows_count + 1):
         matrix.append([int(element) for element in input("Enter {} row of {}matrix, in format"
                                                          " B1,B2,...Bn where each 'B' is matrix"
                                                          " element: ".format(row_num, matrix_name)).split(",")])
     return [matrix, matrix_rows_count, matrix_columns_count]
+
+
+def get_answer_from_vectors_menu(chosen_option):
+    given_vector1 = get_vector_from_user("first ")
+    if chosen_option == "1":
+        return "Vector length: {}".format(get_vector_length(given_vector1))
+    elif chosen_option == "2":
+        given_vector2 = get_vector_from_user("second ")
+        return "scalar product: {}".format(get_scalar_product(given_vector1, given_vector2))
+    elif chosen_option == "3":
+        given_vector2 = get_vector_from_user("second ")
+        return "Angle between vectors: {} degrees".format(get_angle_between_vectors(given_vector1, given_vector2))
+
+
+def get_answer_from_matrices_menu(chosen_option):
+    matrix1_info = get_matrix_info_from_user("first ")
+    if chosen_option == "1":
+        transposed = get_transposed_matrix(matrix1_info[0], matrix1_info[1], matrix1_info[2])
+        return transposed
+    elif chosen_option == "2":
+        print("Both matrices must be the same size!")
+        matrix2_info = get_matrix_info_from_user("second ")
+        sum_matrix = get_matrices_sum(matrix1_info[0], matrix2_info[0], matrix1_info[1], matrix1_info[2])
+        return sum_matrix
+    elif chosen_option == "3":
+        print("The number of columns in the first matrix must match the number of rows in the second matrix!")
+        matrix2_info = get_matrix_info_from_user("second ")
+        product_matrix = get_matrices_product(matrix1_info[0], matrix2_info[0], matrix1_info[1],
+                                              matrix1_info[2],
+                                              matrix2_info[1], matrix2_info[2])
+        return product_matrix
 
 
 if __name__ == "__main__":
@@ -63,53 +84,23 @@ if __name__ == "__main__":
     while condition != "exit":
         if condition == "welcome":
             condition = input("Select what you want to operate with:\n1.Vectors\n2.Matrices\n3.Nothing\n")
-
         elif condition == "1":
-            action = input("Select operation:\n1.Define vector length\n2.Calculate dot product\n3.Define angle between "
-                           "vectors\n4.Back\n")
-            if action == "1":
-                given_vector = get_vector_from_user()
-                print("Vector length: {}".format(get_vector_length(given_vector)))
-            elif action == "2":
-                given_vector1 = get_vector_from_user("first ")
-                given_vector2 = get_vector_from_user("second ")
-                print("Dot product: {}".format(get_dot_product(given_vector1, given_vector2)))
-            elif action == "3":
-                given_vector1 = get_vector_from_user("first ")
-                given_vector2 = get_vector_from_user("second ")
-                print("Angle between vectors: {} degrees".format(get_angle_between_vectors(given_vector1, given_vector2)))
-            elif action == "4":
+            action = input("Select operation:\n1.Define vector length\n2.Calculate scalar product\n3.Define angle "
+                           "between vectors\n4.Back\n")
+            if action == "4":
                 condition = "welcome"
-
+                continue
+            print(get_answer_from_vectors_menu(action))
         elif condition == "2":
             action = input("Select operation:\n1.Transpose matrix\n2.Calculate sum of matrices \n3.Calculate"
                            " product of matrices\n4.Back\n")
-            if action == "1":
-                matrix_info = get_matrix_info()
-                transposed = get_transposed_matrix(matrix_info[0], matrix_info[1], matrix_info[2])
-                print("Transposed matrix:")
-                for row in transposed:
-                    print(row)
-            elif action == "2":
-                print("Both matrices must be the same size!")
-                matrix1_info = get_matrix_info("first ")
-                matrix2_info = get_matrix_info("second ")
-                sum_matrix = get_matrices_sum(matrix1_info[0], matrix2_info[0], matrix1_info[1], matrix1_info[2])
-                print("Addition result:")
-                for row in sum_matrix:
-                    print(row)
-            elif action == "3":
-                print("The number of columns in the first matrix must match the number of rows in the second matrix!")
-                matrix1_info = get_matrix_info("first ")
-                matrix2_info = get_matrix_info("second ")
-                product_matrix = get_matrices_product(matrix1_info[0], matrix2_info[0], matrix1_info[1], matrix1_info[2],
-                                                      matrix2_info[1], matrix2_info[2])
-                print("Product:")
-                for row in product_matrix:
-                    print(row)
-            elif action == "4":
+            if action == "4":
                 condition = "welcome"
-
+                continue
+            calculated_matrix = get_answer_from_matrices_menu(action)
+            print("Result:")
+            for row in calculated_matrix:
+                print(row)
         elif condition == "3":
             print("Bye.")
             condition = "exit"
