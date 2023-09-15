d881f94544d5b37016e82419f5d67b26bf4ea27a
CandiedDisc192 <ermodeti@gmail.com>
2023-09-15 18:27:27 +0300

    modified Task3

--- 3.py
+++ 3.py
@@ -1,82 +1,83 @@
 import sys
-from os.path import getsize
 
 
-def wc(file_name, is_lines=True, is_words=True, is_bytes=True, is_count=False):
-    result = ""
-    file = open(file_name)
-    lines = file.readlines()
-    file.close()
-    if is_lines:
-        result += str(len(lines)) + " "
-    if is_words:
-        words_count = 0
-        for line in lines:
-            words_count += len(line.split())
-        result += str(words_count) + " "
-    if is_bytes:
-        result += str(getsize(file_name))
-    if is_count:
-        chars_count = 0
-        for line in lines:
-            chars_count += len(line) if line != "\n" else 0
-        result += str(chars_count)
-    print(result[:-1])
+def wc(file_name):
+    result = {"-l": 0, "-w": 0, "-c": 0, "-m": 0}
+    with open(file_name, "r") as file:
+        file_encoding = file.encoding
+        for line in file:
+            result["-l"] += 1
+            result["-w"] += len(line.split())
+            result["-m"] += len(line)
+            result["-c"] += len(line.encode(file_encoding)) + (line[-1] == "\n")
+    return result
 
 
-def head_or_tail_n(file_name, head, n=10):
+def head_or_tail_n(file_name, is_head, n=10):
     file = open(file_name, "r")
     lines = [line for line in file]
+    lines = lines[::-1] if not is_head else lines
     file.close()
     len_lines = len(lines)
     n = len_lines if len_lines < n else n
-    for line in lines if head else lines[len_lines - n:]:
-        print(line.rstrip())
-        n -= 1
-        if n <= 0:
-            break
+    result = []
+    for i in range(n):
+        result.append(lines[i])
+    return "".join(result) if is_head else "".join(result[::-1])
 
 
-def head_or_tail_c(file_name, head, c):
+def head_or_tail_c(file_name, is_head, c):
     file = open(file_name, "r")
     lines = [line for line in file]
     file.close()
-    lines = lines[::-1] if not head else lines
+    lines = lines[::-1] if not is_head else lines
     file_encoding = file.encoding
     file.close()
     current_bytes = 0
     res = ""
-    is_all_bytes_taken = False
     for line in lines:
-        if is_all_bytes_taken:
+        line = line[::-1] if not is_head else line
+        encoded_line = line.encode(file_encoding)
+        bytes_taken_by_line = len(encoded_line) + ("\n" in line)
+        if current_bytes + bytes_taken_by_line > c:
+            for symbol in line:
+                symbol_bytes = len(symbol.encode(file_encoding)) + (symbol == "\n")
+                if current_bytes + symbol_bytes <= c:
+                    res += symbol
+                    current_bytes += symbol_bytes
+                else:
+                    break
             break
-        for symbol in line if head else line[::-1]:
-            current_symbol_bytes = len(symbol.encode(file_encoding)) if symbol != "\n" else 2
-            if current_bytes + current_symbol_bytes <= c:
-                res += symbol
-                current_bytes += current_symbol_bytes
-            else:
-                is_all_bytes_taken = True
-                break
-    print(res) if head else print(res[::-1])
+        else:
+            res += line
+            current_bytes += bytes_taken_by_line
+    return res if is_head else res[::-1]
 
 
 if __name__ == "__main__":
     input_info = sys.argv[1:]
     command = input_info[0]
-    parameters = input_info[1:-1]
+    user_parameters = input_info[1:-1]
     given_file = input_info[-1]
+
     if command == "wc":
-        if parameters:
-            wc(given_file, "-l" in parameters, "-w" in parameters, "-c" in parameters, "-m" in parameters)
+        wc_result = wc(given_file)
+        if user_parameters:
+            order = ["-l", "-w", "-c", "-m"]
+            output = ""
+            for parameter in order:
+                if parameter in user_parameters:
+                    print(wc_result[parameter], end=" ")
         else:
-            wc(given_file)
+            print(wc_result["-l"], wc_result["-w"], wc_result["-c"])
+
     elif command == "head" or command == "tail":
-        if "-n" in parameters:
-            head_or_tail_n(given_file, command == "head", int(parameters[-1]))
-        elif "-c" in parameters:
-            head_or_tail_c(given_file, command == "head", int(parameters[-1]))
-        if not parameters:
-            head_or_tail_n(given_file, command == "head")
+        if "-n" in user_parameters:
+            print(head_or_tail_n(given_file, command == "head", int(user_parameters[-1])))
+        elif "-c" in user_parameters:
+            print(head_or_tail_c(given_file, command == "head", int(user_parameters[-1])))
+        if not user_parameters:
+            print(head_or_tail_n(given_file, command == "head"))
+
     else:
         print("Unknown command")
