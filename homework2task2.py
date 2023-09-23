3c93bfaadaaf58fc84be7e545a79676602f7d709
CandiedDisc192 <ermodeti@gmail.com>
2023-09-23 17:10:27 +0300

    Homework 2. Task 2. Ermolovich

---
+++ homework2task2.py
@@ -0,0 +1,29 @@
+import csv
+
+
+def determine_word_frequency(file_name, file_encoding="utf-8"):
+    word_to_frequency = dict()
+    with open(file_name, "r", encoding=file_encoding) as f_in:
+        for line in f_in:
+            split_line = line.lower().split()
+            for word in split_line:
+                word_to_frequency[word] = word_to_frequency[word] + 1 if (word in word_to_frequency) else 1
+    return word_to_frequency
+
+
+def write_csv_word_frequency(file_name, dict_data, file_encoding="utf-8"):
+    with open(file_name, "w", encoding=file_encoding) as csv_f_in:
+        writer = csv.writer(csv_f_in, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(["Слово", "Частота Встречаемости"])
+        for word in dict_data:
+            writer.writerow([word, dict_data[word]])
+
+
+if __name__ == "__main__":
+    user_txt_file_name = input("Введите путь до txt файла, в котором нужно  посчитать частоту встречаемости каждого"
+                               " уникального слова: ")
+    user_encoding = input("Введите кодировку этого файла: ")
+    data = determine_word_frequency(user_txt_file_name, user_encoding)
+    user_csv_file_name = input("Введите путь до пустого csv файла, в который будет записан результат (если файла нет, "
+                               "то он будет создан): ")
+    write_csv_word_frequency(user_csv_file_name, data, user_encoding)
