48ed221fdd1eba157a15f336351f93cd56dab1cd
CandiedDisc192 <ermodeti@gmail.com>
2023-09-24 17:50:19 +0300

    change homework2task2

--- homework2task2.py
+++ homework2task2.py
@@ -1,4 +1,5 @@
 import csv
+from os.path import exists
 
 
 def determine_word_frequency(file_name, file_encoding="utf-8"):
@@ -7,7 +8,7 @@ def determine_word_frequency(file_name, file_encoding="utf-8"):
         for line in f_in:
             split_line = line.lower().split()
             for word in split_line:
-                word_to_frequency[word] = word_to_frequency[word] + 1 if (word in word_to_frequency) else 1
+                word_to_frequency[word] = word_to_frequency.get(word, 0) + 1
     return word_to_frequency
 
 
@@ -15,15 +16,16 @@ def write_csv_word_frequency(file_name, dict_data, file_encoding="utf-8"):
     with open(file_name, "w", encoding=file_encoding) as csv_f_in:
         writer = csv.writer(csv_f_in, quoting=csv.QUOTE_NONNUMERIC)
         writer.writerow(["Слово", "Частота Встречаемости"])
-        for word in dict_data:
-            writer.writerow([word, dict_data[word]])
+        writer.writerows(dict_data.items())
 
 
 if __name__ == "__main__":
     user_txt_file_name = input("Введите путь до txt файла, в котором нужно  посчитать частоту встречаемости каждого"
                                " уникального слова: ")
-    user_encoding = input("Введите кодировку этого файла: ")
-    data = determine_word_frequency(user_txt_file_name, user_encoding)
-    user_csv_file_name = input("Введите путь до пустого csv файла, в который будет записан результат (если файла нет, "
-                               "то он будет создан): ")
-    write_csv_word_frequency(user_csv_file_name, data, user_encoding)
+    if exists(user_txt_file_name):
+        data = determine_word_frequency(user_txt_file_name)
+        user_csv_file_name = input("Введите путь до пустого csv файла, в который будет записан результат (если файла"
+                                   " нет, то он будет создан): ")
+        write_csv_word_frequency(user_csv_file_name, data)
+    else:
+        print("txt файл не найден")
