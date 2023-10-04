9a18a0b499985b3a59cbe5b12937ae81adf2d203
CandiedDisc192 <ermodeti@gmail.com>
2023-10-04 20:04:19 +0300

    final change of task3

--- src/homeworks/homework2/task3.py
+++ src/homeworks/homework2/task3.py
@@ -1,30 +1,31 @@
 from os.path import exists
 
 
+def get_current_dna(origin_dna, current_log):
+    command, arg1, arg2 = current_log
+    if command == "DELETE":
+        start_index = origin_dna.find(arg1)
+        end_index = origin_dna.find(arg2, start_index + len(arg1)) + len(arg2)
+        origin_dna = origin_dna[:start_index] + origin_dna[end_index:]
+    elif command == "INSERT":
+        insert_index = origin_dna.find(arg1) + len(arg1)
+        origin_dna = origin_dna[:insert_index] + arg2 + origin_dna[insert_index:]
+    elif command == "REPLACE":
+        origin_dna = origin_dna.replace(arg1, arg2, 1)
+    return origin_dna
+
+
 def transform_logs(logs_file_path, transformed_logs_file_path):
-    transformed_text = ""
+    transformed_lines = []
     with open(logs_file_path, "r", encoding="utf-8") as logs_f_in:
-        origin_dna_length, dna, logs_count = (
-            int(logs_f_in.readline()),
-            logs_f_in.readline().rstrip(),
-            int(logs_f_in.readline()),
-        )
+        _ = int(logs_f_in.readline())
+        dna = logs_f_in.readline().rstrip()
+        logs_count = int(logs_f_in.readline())
         for raw_log_i in range(logs_count):
-            log = logs_f_in.readline().split()
-            if log[0] == "DELETE":
-                start_index = dna.find(log[1])
-                end_index = dna.find(log[2], start_index + len(log[1])) + len(log[2])
-                dna = dna[:start_index] + dna[end_index:]
-            elif log[0] == "INSERT":
-                insert_index = dna.find(log[1]) + len(log[1])
-                dna = dna[:insert_index] + log[2] + dna[insert_index:]
-            elif log[0] == "REPLACE":
-                dna = dna.replace(log[1], log[2], 1)
-            transformed_text += dna + "\n" if raw_log_i != logs_count - 1 else dna
-        with open(
-            transformed_logs_file_path, "w", encoding="utf-8"
-        ) as transforming_file:
-            transforming_file.write(transformed_text)
+            dna = get_current_dna(dna, logs_f_in.readline().split())
+            transformed_lines.append(dna + "\n" if raw_log_i != logs_count - 1 else dna)
+    with open(transformed_logs_file_path, "w", encoding="utf-8") as transforming_file:
+        transforming_file.writelines(transformed_lines)
 
 
 if __name__ == "__main__":
