133157a681203d1be9aa7cfd937357ef7a36cdc0
CandiedDisc192 <ermodeti@gmail.com>
2023-10-14 01:05:03 +0300

    fix: guess cant start with 0, guess contains only unique numbers

--- src/practices/practice3/task2.py
+++ src/practices/practice3/task2.py
@@ -11,18 +11,47 @@ def get_bulls_and_cows_current_res(current_guess, answer):
     return bulls, cows
 
 
+def is_contains_duplicates(string):
+    for symbol in string:
+        if string.count(symbol) > 1:
+            return True
+    return False
+
+
+def get_digits_count():
+    digits_number = input("Введите количество загаданных цифр: ")
+    while digits_number not in (str(n) for n in range(1, 10 + 1)):
+        digits_number = input("Введите количество загаданных цифр (число от 1 до 10): ")
+    return int(digits_number)
+
+
+def generate_random_value(digits_number):
+    random_val = "".join(sample("1234567890", digits_number))
+    return random_val if random_val[0] != "0" else random_val[::-1]
+
+
+def is_guess_valid(cur_guess, digits_number):
+    return (
+        cur_guess.isdigit()
+        and len(cur_guess) == digits_number
+        and not is_contains_duplicates(cur_guess)
+    )
+
+
 if __name__ == "__main__":
-    digits_count = int(input("Введите количество загаданных цифр (число от 1 до 10): "))
+    digits_count = get_digits_count()
     print('Чтобы закончить игру напишите "стоп"')
-    random_value = "".join(sample("1234567890", digits_count))
+    random_value = generate_random_value(digits_count)
     current_guess_num = 1
     while True:
         guess = input(f"{current_guess_num}. Введите ваше предположение: ")
         if guess == "стоп":
             print(f"Вы проиграли, загаданное значение: {random_value}")
             break
-        if not guess.isdigit() or len(guess) != digits_count:
-            print(f"Введите {digits_count}-значное число")
+        if not is_guess_valid(guess, digits_count):
+            print(
+                f"Введите {digits_count}-значное число, не содержащее повторяющихся цифр"
+            )
             continue
         current_bulls, current_cows = get_bulls_and_cows_current_res(
             guess, random_value
@@ -30,6 +59,5 @@ if __name__ == "__main__":
         if current_bulls == digits_count:
             print("Вы победили!")
             break
-        else:
-            print(f"Быков: {current_bulls}, коров: {current_cows}")
-            current_guess_num += 1
+        print(f"Быков: {current_bulls}, коров: {current_cows}")
+        current_guess_num += 1
