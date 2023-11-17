03b39fff78e3d1f4f9803f5d692716a6b49fd572
Vasya <ermodeti@gmail.com>
2023-11-18 01:58:59 +0300

    fix: renaming, + in decimal repr, add more annotations, new function add_leading_digits, change invert_bin

--- src/homeworks/homework4/task1.py
+++ src/homeworks/homework4/task1.py
@@ -1,9 +1,13 @@
-def clean_leading_trash(bin_num: str):
+def add_leading_digits(bin_num: str, digits_count: int) -> str:
+    return bin_num[0] * digits_count + bin_num
+
+
+def delete_leading_digits(bin_num: str) -> str:
     sign_digit = bin_num[0]
     return sign_digit + bin_num.lstrip(sign_digit)
 
 
-def get_positive_bin(num: int):
+def get_positive_bin(num: int) -> str:
     result = ""
     num = abs(num)
     while num != 0:
@@ -12,50 +16,48 @@ def get_positive_bin(num: int):
     return result if result else "0"
 
 
-def invert_bin(bin_num: str):
-    inverted = ""
-    for digit in bin_num:
-        inverted += "0" if digit == "1" else "1"
-    return inverted
+def invert_bin(bin_num: str) -> str:
+    return "".join(map(lambda x: str(int(x == "0")), bin_num))
 
 
-def get_opposite_bin(bin_num: str):
+def get_opposite_bin(bin_num: str) -> str:
     return get_bin_sum(invert_bin(bin_num), "01")
 
 
-def get_bin_sum(bin1: str, bin2: str):
+def get_bin_sum(max_bin: str, min_bin: str) -> str:
     outcomes = ("00", "01", "10", "11")
-    bin2, bin1 = sorted((bin1, bin2), key=len)
-    bin2 = (len(bin1) - len(bin2)) * bin2[0] + bin2
-    res, previous_overflow = "", "0"
-    for i in range(len(bin1) - 1, -1, -1):
-        cur_sum = int(previous_overflow) + int(bin1[i]) + int(bin2[i])
-        previous_overflow = outcomes[cur_sum][0]
-        res += outcomes[cur_sum][1]
-    res = res[::-1] if bin1[0] != bin2[0] else previous_overflow + res[::-1]
-    return clean_leading_trash(res)
+    min_bin, max_bin = sorted((max_bin, min_bin), key=len)
+    min_bin = add_leading_digits(min_bin, len(max_bin) - len(min_bin))
+    sum_bin, previous_overflow = "", "0"
+    for i in range(len(max_bin) - 1, -1, -1):
+        digits_sum = int(previous_overflow) + int(max_bin[i]) + int(min_bin[i])
+        previous_overflow = outcomes[digits_sum][0]
+        sum_bin += outcomes[digits_sum][1]
+    sum_bin = (
+        sum_bin[::-1] if max_bin[0] != min_bin[0] else previous_overflow + sum_bin[::-1]
+    )
+    return delete_leading_digits(sum_bin)
 
 
-def get_twos_compliment(num: int):
-    sign = num < 0
-    casual_bin = "0" + get_positive_bin(num)
-    if sign:
-        return get_opposite_bin(casual_bin)
-    return casual_bin
+def get_twos_complement(num: int) -> str:
+    positive_binary = "0" + get_positive_bin(num)
+    if num < 0:
+        return get_opposite_bin(positive_binary)
+    return positive_binary
 
 
-def get_decimal_representation(bin_num: str):
+def get_decimal_representation(bin_num: str) -> str:
     sign = int(bin_num[0])
     bin_num = bin_num if not sign else get_opposite_bin(bin_num)
     absolute_value = 0
-    degree = 0
+    order = 0
     for digit in bin_num[::-1]:
-        absolute_value += int(digit) * (2**degree)
-        degree += 1
-    return int(f"{'-' if sign else ''}{absolute_value}")
+        absolute_value += int(digit) * (2**order)
+        order += 1
+    return f"{'-' if sign else '+'}{absolute_value}"
 
 
-def main():
+def main() -> None:
     try:
         user_num1 = int(input("Enter first integer: "))
         user_num2 = int(input("Enter second integer: "))
@@ -63,14 +65,12 @@ def main():
         print("Input integer numbers.")
         return
 
-    processed_num1, processed_num2 = get_twos_compliment(
-        user_num1
-    ), get_twos_compliment(user_num2)
-    bin_sum = get_bin_sum(processed_num1, processed_num2)
-    bin_difference = get_bin_sum(processed_num1, get_opposite_bin(processed_num2))
+    num1, num2 = get_twos_complement(user_num1), get_twos_complement(user_num2)
+    bin_sum = get_bin_sum(num1, num2)
+    bin_difference = get_bin_sum(num1, get_opposite_bin(num2))
     print(
-        f"Two's complement for 1st number: {processed_num1}\n"
-        f"Two's complement for 2nd number: {processed_num2}\n"
+        f"Two's complement for 1st number: {num1}\n"
+        f"Two's complement for 2nd number: {num2}\n"
         f"Sum (base 2): {bin_sum}\n"
         f"Sum (base 10): {get_decimal_representation(bin_sum)}\n"
         f"Difference (base 2): {bin_difference}\n"
