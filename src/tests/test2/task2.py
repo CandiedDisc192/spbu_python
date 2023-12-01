adaf06a885bbefe098126d32c9713fc2a0a11f46
Vasya <ermodeti@gmail.com>
2023-12-01 15:54:56 +0300

    black

--- src/tests/test2/task2.py
+++ src/tests/test2/task2.py
@@ -18,7 +18,7 @@ def logger(logs_path):
                 for key in raw_arguments.arguments["kwargs"]:
                     arguments += f"{key}={raw_arguments.arguments['kwargs'][key]} "
 
-            time = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+            time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
             res = function(*args, **kwargs)
             with open(logs_path, "a") as logs_file:
                 logs_file.write(f"{time} {f_name} {arguments.rstrip()} {res}\n")
